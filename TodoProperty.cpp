c6218fb376f7358c4259f28fee9422c92a6d7add
httpak <httpak@connect.ust.hk>
2017-04-10 03:03:58 +0800

    Add files via upload

---
+++ TodoProperty.cpp
@@ -0,0 +1,73 @@
+/*
+ * This should contain the implementation of TodoPropertyModel.
+ */
+
+//You need to submit this file
+//You need to modify this file
+
+//your code goes here
+
+#include "TodoProperty.h"
+#include "TodoPlayer.h"
+#include "TodoBoard.h"
+#include <sstream>
+
+using namespace std;
+
+TodoPropertyModel::TodoPropertyModel(string name, int price, Color color) : PropertyModel(name, price, color){
+
+}
+
+TodoPropertyModel::~TodoPropertyModel() {
+
+}
+
+void TodoPropertyModel::action(PlayerModel* player, const BoardController& board){
+	if(owner == NULL){
+		//allow to buy
+		int option;
+
+		stringstream yes_no;
+		yes_no << "Do you want to buy " << name <<"?";
+		const string options[2] = {"Yes" , "No"};
+
+		option = board.requestInput(yes_no.str(), options, 2);
+		if(option == 0){
+			if(player->getCash() >= this->getRate()){
+				player->gainProperty(this);
+				this->owner = player;
+				this->color = player->getColor();
+				string s;
+				s = player->getName() + " bought " + name;
+				board.prompt(s);
+			}else{
+				board.prompt("You don't have enough money to buy it.");
+			}
+		}
+	}else if(owner == player){
+		if(houses < MAX_HOUSES){
+			if(player->getCash() >= BUILDING_COST){
+				houses += 1;
+				player->pay(BUILDING_COST);
+			}
+		}
+	}else{
+		player->pay(this->getRate());
+		this->getOwner()->collect(this->getRate());
+		string s;
+		s = player->getName() + " pays " + this->getOwner()->getName() + " $" + static_cast<ostringstream*>( &(ostringstream() << this->getRate()) )->str();
+		board.prompt(s);
+	}
+	string s;
+	s = player->getName() + ": this round complete.";
+	board.prompt(s);
+}
+
+
+int TodoPropertyModel::getRate() const {
+	if(owner == NULL){
+		return price;
+	}else{
+		return price * ( houses + 1)  * 3 / 4;
+	}
+}
---
+++ TodoRestaurant.h
@@ -0,0 +1,84 @@
+/*
+ * This should contain the class of TodoRestaurantModel.
+ */
+
+//You need to submit this file
+//You need to modify this file
+
+#ifndef TODORESTAURANTMODEL_H_
+#define TODORESTAURANTMODEL_H_
+
+//your code goes here
+
+#include "TodoProperty.h"
+#include "param.h"
+
+class TodoPlayerModel;
+class TodoBoardController;
+/**
+ *  This is a model class that inherits a cell. A property should be able to be owned.
+ *  When a property is owned by a player, any other player stepping on this property
+ *  will be charged by some cash, according to the rate calculated.
+ */
+
+class TodoRestaurantModel : public TodoPropertyModel {
+
+public:
+	/**
+	 * Constructor
+	 *
+	 * @param name: the name of this property.
+	 * @param price: the selling price of this property.
+	 * @param color: the initial color of this property.
+	 */
+	TodoRestaurantModel(string name, int price, Color color);
+
+	/**
+	 * Destructor.
+	 */
+	virtual ~TodoRestaurantModel();
+
+	/**
+	 * The abstract function implemented in this class. When a player steps on it, he can buy it if
+	 * it is not previously owned. He need to pay for the rent if it is owned by other player. He can build
+	 * a house if he owned this property before this function is invoked. When there are five houses on
+	 * this property, no more house can be built.
+	 *
+	 * @param player: The player who is currently visiting this property.
+	 * @param board: The board controller.
+	 */
+	virtual void action(PlayerModel* player, const BoardController& board);
+
+	/**
+	 * If the property is not owned by any player. The rate is set as the selling price of this property.
+	 * When the property is owned by a player, the rate is the rent which is calculated by the
+	 * formula: (selling price) * ( number of houses + 1) * 3 / 4;
+	 *
+	 * @return the selling price or the rate of the rent.
+	 */
+	virtual int getRate() const;
+
+
+//	/**
+//	 * The getter function of the owner of this property.
+//	 */
+//	PlayerModel* getOwner() const {return owner;}
+
+	void  getRestaurantCount() const;
+
+
+protected:
+
+//	/**
+//	 * The owner of this property.
+//	 */
+//	PlayerModel* owner;
+
+	/**
+	 * The owner of this property.
+	 */
+	TodoPlayerModel* todoOwner;
+
+};
+
+#endif /* TODORESTAURANTMODEL_H_ */

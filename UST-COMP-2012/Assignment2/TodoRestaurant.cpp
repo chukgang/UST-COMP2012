73352413f67156387e09d19fb0d4bbe5fe3d30d7
Christopher Wong <christopherpk1996@gmail.com>
2017-04-14 19:49:45 +0800

    bugfix restaurant rate

--- UST-COMP-2012/Assignment2/TodoRestaurant.cpp
+++ UST-COMP-2012/Assignment2/TodoRestaurant.cpp
@@ -5,7 +5,6 @@
 //You need to submit this file
 //You need to modify this file
 //your code goes here
-
 /* code here */
 
 #include "TodoRestaurant.h"
@@ -61,17 +60,24 @@ void TodoRestaurantModel::action(PlayerModel* player, const BoardController& boa
 
 int TodoRestaurantModel::getRate() const
 {
-	int count = 0;
-	for (int i = 0; i != this->owner->getPropertyCount(); i++)
+	if (this->owner != NULL)
 	{
-		const PropertyModel* property = this->owner->getProperties()[i];
-		const TodoRestaurantModel* restaurant = dynamic_cast<const TodoRestaurantModel*>(property);
-		if (restaurant != NULL)
+		int count = 0;
+		for (int i = 0; i != this->owner->getPropertyCount(); i++)
 		{
-			count++;
+			const PropertyModel* property = this->owner->getProperties()[i];
+			const TodoRestaurantModel* restaurant = dynamic_cast<const TodoRestaurantModel*>(property);
+			if (restaurant != NULL)
+			{
+				count++;
+			}
 		}
+		return count * this->price;
 	}
-	return count * this->price;
+	else
+	{
+		return this->price;
+	}
 }
 
 /* code end */

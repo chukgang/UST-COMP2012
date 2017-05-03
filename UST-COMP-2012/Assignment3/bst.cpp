338d577598dec25e7f5eed53598b226ac36378f6
Christopher Wong <christopherpk1996@gmail.com>
2017-05-03 23:41:16 +0800

    bst

--- UST-COMP-2012/Assignment3/bst.cpp
+++ UST-COMP-2012/Assignment3/bst.cpp
@@ -16,6 +16,25 @@ template <typename T, typename K>
 BT<T,K>* BST<T,K>::search(const K& k)
 {
     //write your codes here
+	/* code here */
+	node* tar = this->root;
+	while (tar != NULL)
+	{
+		if (tar->key == k)
+		{
+			return tar;
+		}
+		if (tar->key < k)
+		{
+			tar = tar->left;
+		}
+		else
+		{
+			tar = tar->right;
+		}
+	}
+	return NULL;
+	/* code end */
 }
 
 
@@ -27,6 +46,14 @@ template <typename T, typename K>
 BT<T,K>* BST<T,K>::find_min()
 {
     //write your codes here
+	/* code here */
+	node* tar = this->root;
+	while (tar->left != NULL)
+	{
+		tar = tar->left;
+	}
+	return tar;
+	/* code end */
 }
 
 
@@ -37,6 +64,35 @@ template <typename T, typename K>
 void BST<T,K>::insert(const T& x, const K& k)
 {
     //write your codes here
+	/* code here */
+	node* tar = this->root;
+	while (true)
+	{
+		if (tar->key == k)
+		{
+			return;
+		}
+		if (tar->key < k)
+		{
+			if (tar->left == NULL)
+			{
+				tar->left = new node(x, k);
+				return;
+			}
+			tar = tar->left;
+		}
+		else
+		{
+			if (tar->right == NULL)
+			{
+				tar->right = new node(x, k);
+				return;
+			}
+			tar = tar->right;
+		}
+	}
+	return NULL;
+	/* code end */
 }
 
 
@@ -47,6 +103,9 @@ template <typename T, typename K>
 void BST<T,K>::remove(const K& k)
 {    
     //write your codes here
+	/* code here */
+
+	/* code end */
 }
 
 #endif /* BST_CPP */

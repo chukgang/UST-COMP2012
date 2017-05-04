c7ce305c54e9284fc8207fb58273735b77230b19
WONG Ping Kin Christopher <pkcwongab@WIN2K.CS.UST.HK>
2017-05-04 14:18:42 +0800

    clean lab9

---
+++ UST-COMP-2012/lab9/BTreeExercises.h
@@ -0,0 +1,27 @@
+//
+// Operations on Binary Tree
+//
+
+#ifndef LAB9_BTREEEXERCISES_H
+#define LAB9_BTREEEXERCISES_H
+
+#include <iostream>
+#include <queue>
+#include "BtreeNode.h"
+
+template<class T>
+int treeHeight(BtreeNode<T> *root);
+
+template<class T>
+int countNodes(BtreeNode<T> *root);
+
+template<class T>
+void printLevelOrder(BtreeNode<T> *root);
+
+template<class T>
+BtreeNode<T> *mirror(BtreeNode<T> *root);
+
+template<class T>
+bool isComplete(BtreeNode<T> *root);
+
+#endif //LAB9_BTREEEXERCISES_H

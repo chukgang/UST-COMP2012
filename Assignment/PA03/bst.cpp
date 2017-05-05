afba3cb33536e548357c548d34d434d81fc06737
PAK Ho Tin Timothy <httpak@WIN2K.CS.UST.HK>
2017-05-05 19:57:21 +0800

    Updated functions of BST

--- Assignment/PA03/bst.cpp
+++ Assignment/PA03/bst.cpp
@@ -15,20 +15,23 @@
 template <typename T, typename K>
 BT<T,K>* BST<T,K>::search(const K& k){
     //write your codes here
-	stack<typename T, typename K> searchKey;
-	searchKey.push(root);
-	while(!searchKey.empty()){
-		if(k > searchKey.top()->key){
-			searchKey.push(searchKey.top()->right_subtree());
-		}else if(k < searchKey.top->key){
-			searchKey.push(searchKey.top()->left_subtree());
-		}else{
-			return searchKey.top();
-		}
-	}
-	return NULL;
+	return searchItem(root, k);
 }
 
+template <typename T, typename K>
+BT<T,K>* BST<T,K>::searchItem(BST<T, K>* root, const K& k){
+    //write your codes here
+	if(root == NULL){
+		return NULL;
+	}
+	if(k > root->key){
+		return searchItem(root->right_subtree(), k);
+	}else if(k < root->key){
+		return searchItem(root->left_subtree(), k);
+	}else{
+		return root;
+	}
+}
 
 /* TODO
  * Goal: To find the minimum node stored in a BST tree.
@@ -37,21 +40,24 @@ BT<T,K>* BST<T,K>::search(const K& k){
 template <typename T, typename K>
 BT<T,K>* BST<T,K>::find_min(){
     //write your codes here
-	if(root != NULL){
-		node* minimum = root;
-	}else{
-		return;
+	findMinItme(root);
+}
+
+template <typename T, typename K>
+BT<T,K>* BST<T,K>::findMinItem(BST<T, K>* root){
+    //write your codes here
+	if(root == NULL){
+		return NULL;
 	}
-	while(ture){
-		if(minimum->left_subtree() != 0){
-			minimum = minimum->left_subtree();
-		}else if(minimum->right_subtree() != 0){
-			minimum = minimum->right_subtree();
-		}else{
-			return minimum;
-		}
+	if(root->left_subtree() != NULL){
+		findMinItem(root->left_subtree());
+	}else if(root->right_subtree() != NULL){
+		findMinItem(root->right_subtree());
+	}else{
+		return root;
 	}
 }
+
 
 
 /* TODO
@@ -95,32 +101,36 @@ void BST<T,K>::insert(const T& x, const K& k){
 template <typename T, typename K>
 void BST<T,K>::remove(const K& k){
     //write your codes here
-	if(root != NULL){
-		node* removeItem = root;
-	}else{
-		return;
+	if(searchItem(root, k) != NULL){
+		root = removeItem(root, k);
 	}
-	while(ture){
-		if(k > removeItem->key){
-			if(removeItem->right_subtree() != NULL){
-				removeItem = removeItem->right_subtree();
-			}else{
-				return;
-			}
-		}else if(k < removeItem->key){
-			if(removeItem->left_subtree() != NULL){
-				removeItem = removeItem->left_subtree();
-			}else{
-				return;
-			}
-		}else{
-
-			return;
-		}
-	}
-
 }
 
+template <typename T, typename K>
+void BST<T,K>::removeItem(BST<T, K>* root, const K& k){
+    //write your codes here
+	if(root == NULL){
+		return NULL;
+	}else if(k > root->key){
+		removeItem(root->right_subtree(), k);
+	}else if(k < root->key){
+		removeItem(root->left_subtree(), k);
+	}else{
+		BST<T, K>* temp = root;
+		if(root->right_subtree() == NULL){
+			root = root->left_subtree();
+			delete temp;
+		}else if(root->left_subtree() == NULL){
+			root = root->right_subtree();
+			delete temp;
+		}else{
+			BST<T, K>* minimum = new findMinItem(root->right_subtree());
+			minimum
+			root = minium;
+			delete temp;
+		}
+	}
+}
 
 /* TODO
  * Goal: Clear the node stack and set current pointer to the root 
